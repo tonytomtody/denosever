afd10d4298971bfa2bc55c24b986441ad2c68b71
tonytomtody <tonytomtody@gmail.com>
2024-10-10 18:11:40 +0800

    week3WithDataBase

---
+++ week3/DataBase/03-blog/app.js
@@ -0,0 +1,139 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import * as render from './render.js'
+import { DB } from "https://deno.land/x/sqlite/mod.ts";
+
+const db = new DB("blog.db");
+db.query("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, body TEXT, user TEXT)");
+//db.query(`INSERT INTO posts (title, body, user) VALUES ('config', 'admin', 'default')`);
+
+const router = new Router();
+
+router.get('/', redirectTo)
+  .get('/:user/login/', userLogin)
+  .post('/:user/login/check/', check)
+  .get('/userselect/', userSelect)
+  .get('/signup/', signUp)
+  .post('/signup/confirm/', register)
+  .get('/:user/list/', list)
+  .get('/:user/post/new/', add)
+  .get('/:user/post/:id/', show)
+  .post('/:user/post/', create);
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+function query(sql) {
+  let list = []
+  for (const [id, title, body , user] of db.query(sql)) {
+    list.push({id, title, body , user})
+  }
+  return list
+}
+
+async function userLogin(ctx) {
+  const user = ctx.params.user;
+  ctx.response.body = await render.userLogin(user);
+}
+
+function queryUser() {
+  let list = []
+  for (const [user] of db.query(`SELECT user FROM posts WHERE title == 'config'`)) {
+    list.push({user})
+  }
+  return list
+}
+
+async function signUp(ctx) {
+  ctx.response.body = await render.signUp();
+}
+
+async function redirectTo(ctx) {
+  ctx.response.redirect('/userselect/');
+}
+
+async function list(ctx) {
+  const user = ctx.params.user;
+  let posts = query(`SELECT id, title, body, user FROM posts WHERE user = '${user}' AND title != 'config'`)
+  console.log('list:posts=', posts)
+  ctx.response.body = await render.list(posts,user);
+}
+
+async function userSelect(ctx) {
+  let users = queryUser()
+  console.log('list:users=', users)
+  ctx.response.body = await render.userSelect(users);
+}
+
+async function add(ctx) {
+  const user = ctx.params.user;
+  ctx.response.body = await render.newPost(user);
+}
+
+async function show(ctx) {
+  const user = ctx.params.user;
+  const pid = ctx.params.id;
+  let posts = query(`SELECT id, title, body, user FROM posts WHERE id=${pid}`)
+  let post = posts[0]
+  console.log('show:post=', post)
+  if (!post) ctx.throw(404, 'invalid post id');
+  ctx.response.body = await render.show(post,user);
+}
+
+async function create(ctx) {
+  const user = ctx.params.user;
+  const body = ctx.request.body;
+  if (body.type() === "form") {
+    const pairs = await body.form()
+    const post = {}
+    for (const [key, value] of pairs) {
+      post[key] = value
+    }
+    console.log('create:post=', post)
+    db.query("INSERT INTO posts (title, body, user) VALUES (?, ?, ?)", [post.title, post.body, user]);
+    ctx.response.redirect(`/${user}/list/`);
+  }
+}
+
+async function check(ctx) {
+  const user = ctx.params.user;
+  const body = ctx.request.body;
+  if (body.type() === "form") {
+    const pairs = await body.form()
+    const post = {}
+    for (const [key, value] of pairs) {
+      post[key] = value
+    }
+    console.log('create:post=', post)
+    let list = []
+    for (const [body] of db.query(`SELECT body FROM posts WHERE user = '${user}' AND title == 'config'`)) {
+      list.push({body})
+    }
+    console.log('create:post=', list[0])
+    if (post.body == list[0].body) {
+      ctx.response.redirect(`/${user}/list/`);
+    }
+    else {
+      ctx.response.body = "alert('Invalid Password!')";
+      ctx.response.redirect('/userselect/');
+    }
+  }
+}
+
+async function register(ctx) {
+  const body = ctx.request.body;
+  if (body.type() === "form") {
+    const pairs = await body.form()
+    const post = {}
+    for (const [key, value] of pairs) {
+      post [key] = value
+    }
+    console.log('create:user=', post)
+    db.query("INSERT INTO posts (title, body, user) VALUES (?, ?, ?)", ["config",post.body, post.user]);
+    ctx.response.redirect('/userselect/');
+  }
+}
+
+let port = parseInt(Deno.args[0])
+console.log(`Server run at http://127.0.0.1:${port}`)
+await app.listen({ port });
---
+++ week3/DataBase/03-blog/render.js
@@ -0,0 +1,165 @@
+export function layout(title, content) {
+  return `
+  <html>
+  <head>
+    <title>${title}</title>
+    <style>
+      body {
+        padding: 80px;
+        font: 16px Helvetica, Arial;
+      }
+  
+      h1 {
+        font-size: 2em;
+      }
+  
+      h2 {
+        font-size: 1.2em;
+      }
+  
+      #posts {
+        margin: 0;
+        padding: 0;
+      }
+  
+      #posts li {
+        margin: 40px 0;
+        padding: 0;
+        padding-bottom: 20px;
+        border-bottom: 1px solid #eee;
+        list-style: none;
+      }
+  
+      #posts li:last-child {
+        border-bottom: none;
+      }
+  
+      textarea {
+        width: 500px;
+        height: 300px;
+      }
+  
+      input[type=text],
+      textarea {
+        border: 1px solid #eee;
+        border-top-color: #ddd;
+        border-left-color: #ddd;
+        border-radius: 2px;
+        padding: 15px;
+        font-size: .8em;
+      }
+  
+      input[type=text] {
+        width: 500px;
+      }
+
+      input[type=password],
+      textarea {
+        border: 1px solid #eee;
+        border-top-color: #ddd;
+        border-left-color: #ddd;
+        border-radius: 2px;
+        padding: 15px;
+        font-size: .8em;
+      }
+  
+      input[type=password] {
+        width: 500px;
+      }
+    </style>
+  </head>
+  <body>
+    <section id="content">
+      ${content}
+    </section>
+  </body>
+  </html>
+  `
+}
+
+export function list(posts,user) {
+  let list = []
+  for (let post of posts) {
+    list.push(`
+    <li>
+      <h2>${ post.title }</h2>
+      <p>by ${post.user}</p>
+      <p><a href="/${user}/post/${post.id}/">Read post</a></p>
+    </li>
+    `)
+  }
+  let content = `
+  <h1>Posts</h1>
+  <h2>Login as ${user}</h2>
+  <p><a href="/">Log out</a></p>
+  <p>You have <strong>${posts.length}</strong> posts!</p>
+  <p><a href="/${user}/post/new/">Create a Post</a></p>
+  <ul id="posts">
+    ${list.join('\n')}
+  </ul>
+  `
+  return layout('Posts', content)
+}
+
+export function userSelect(users) {
+  let list = []
+  for (let user of users) {
+    list.push(`
+    <li>
+      <h2><a href="/${user.user}/login/">${user.user}</a></h2>
+    </li>
+    `)
+  }
+  let content = `
+  <h1>Users Select</h1>
+  <p><a href="/signup/">Register a new user</a></p>
+  <ul id="users">
+    ${list.join('\n')}
+  </ul>
+  `
+  return layout('Users Select', content)
+}
+
+export function userLogin(user) {
+  return layout('Log in', `
+    <h1>Log in as ${user}</h1>
+    <p>Welcome Back ${user}!.</p>
+    <form action="check/" method="post">
+      <p><input type="password" placeholder="Password" name="body"></p>
+      <p><input type="submit" value="Log in"></p>
+    </form>
+    `)
+}
+
+export function signUp() {
+  return layout('Sign Up', `
+    <h1>Sign Up</h1>
+    <p>Register a new User.</p>
+    <form action="/signup/confirm/" method="post">
+      <p><input type="text" placeholder="Username" name="user"></p>
+      <p><input type="password" placeholder="Password" name="body"></p>
+      <p><input type="submit" value="Register"></p>
+    </form>
+    `)
+}
+
+export function newPost(user) {
+  return layout('New Post', `
+  <h1>New Post</h1>
+  <p>Create a new post.</p>
+  <form action="/${user}/post/" method="post">
+    <p><input type="text" placeholder="Title" name="title"></p>
+    <p><textarea placeholder="Contents" name="body"></textarea></p>
+    <p><input type="submit" value="Create"></p>
+  </form>
+  `)
+}
+
+export function show(post,user) {
+  return layout(post.title, `
+    <h1>${post.title}</h1>
+    <p>Created by ${post.user}</p>
+    <p>${post.body}</p>
+    <p>View as ${user}</p>
+  `)
+}
