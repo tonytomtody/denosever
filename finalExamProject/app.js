bdc7be06321729a62caf977a96d8666c6d9d4687
tonytomtody <tonytomtody@gmail.com>
2024-12-01 15:57:45 +0800

    post complete

--- finalExamProject/app.js
+++ finalExamProject/app.js
@@ -1,11 +1,10 @@
 import { Application, send, Router } from "https://deno.land/x/oak/mod.ts";
 import { WebSocketServer } from "https://deno.land/x/websocket/mod.ts";
 import { DB } from "https://deno.land/x/sqlite/mod.ts";
-import { sleep } from "groq-sdk/core.mjs";
 
 const db = new DB("blog.db");
 db.query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, nickname TEXT, acount TEXT, password TEXT)");
-db.query("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, body TEXT, user TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, privacy BOOLEAN DEFAULT TRUE)");
+db.query("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, body TEXT, user TEXT, nickname TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, privacy BOOLEAN DEFAULT TRUE)");
 
 // html serve
 const app = new Application();
@@ -36,6 +35,22 @@ wss.on("connection", function (wsc) {
 			case 'register':
 				register(message.user, message.password, message.nickname, wsc);
 				break;
+			case 'changeHTML':
+				switch (message.to) {
+					case 'login':
+						goLogin(wsc);
+						break;
+					case 'register':
+						goRegister(wsc);
+						break;
+					default:
+						goLogin(wsc);
+						break;
+				}
+				break;
+			case 'addpost':
+				addposts(message.title, message.body, message.user, message.privacy, wsc);
+				break;
 			default:
 				console.log('request not found');
 				break;
@@ -46,6 +61,11 @@ wss.on("connection", function (wsc) {
 function register(user, password, nickname, wsc) {
 	console.log('register()...');
 	var sqldata = [];
+	if (user.length == 0 || password.length == 0 || nickname.length == 0) {
+		console.log('empty input');
+		wsc.send(JSON.stringify({ type: 'info', where: 'register', statusinfo: 'empty input' }));
+		return;
+	}
 	try {
 		sqldata = db.query("SELECT * FROM users WHERE acount = ?", [user]);
 		console.log(sqldata);
@@ -58,6 +78,7 @@ function register(user, password, nickname, wsc) {
 		try {
 			db.query("INSERT INTO users (nickname, acount, password) VALUES (?, ?, ?)", [nickname, user, password]);
 			console.log('register success');
+			wsc.send(JSON.stringify({ type: 'info', where: 'unknown', statusinfo: 'goLogin' }));
 			wsc.send(JSON.stringify({ type: 'info', where: 'register', statusinfo: 'success' }));
 		}
 		catch (e) {
@@ -72,10 +93,10 @@ function register(user, password, nickname, wsc) {
 	console.log('register()...end');
 }
 
-function login(user, password, wsc) {
-	var sqldata = [];
+async function login(user, password, wsc) {
+	let sqldata = [];
 	try {
-		sqldata = db.query("SELECT password FROM users WHERE acount = ?", [user]);
+		sqldata = await db.query("SELECT password FROM users WHERE acount = ?", [user]);
 		console.log(sqldata);
 	}
 	catch (e) {
@@ -89,7 +110,8 @@ function login(user, password, wsc) {
 	}
 	else if (sqldata[0] == password) {
 		console.log('login success');
-		wsc.send(JSON.stringify({ type: 'info', where: 'login', statusinfo: 'success' }));
+		let posts = await listpost(user);
+		wsc.send(JSON.stringify({ type: 'info', where: 'unknown', statusinfo: 'goMain', posts: posts }));
 	}
 	else {
 		console.log('login failed');
@@ -98,6 +120,64 @@ function login(user, password, wsc) {
 	console.log('login()...end'); //remove before release
 }
 
+function goLogin(wsc) {
+	console.log('goLogin()...');
+	wsc.send(JSON.stringify({ type: 'info', where: 'unknown', statusinfo: 'goLogin' }));
+	console.log('goLogin()...end');
+}
+
+function goRegister(wsc) {
+	console.log('goRegister()...');
+	wsc.send(JSON.stringify({ type: 'info', where: 'unknown', statusinfo: 'goRegister' }));
+	console.log('goRegister()...end');
+}
+
+function listpost(user){
+	console.log('listpost()...');
+	let posts = [];
+	let sqldata = [];
+	try{
+		sqldata = db.query("SELECT id, title, body, nickname, timestamp FROM posts WHERE user = ? OR privacy = FALSE", [user]);
+	}
+	catch(e){
+		console.log('Error:', e);
+		return posts;
+	}
+	for (const [id, title, body, nickname, timestamp] of sqldata) {
+		posts.push({ id, title, body, nickname, timestamp });
+	}
+	console.log('listpost:posts=', posts);
+	console.log('listpost()...end');
+	return posts;
+}
+
+async function addposts(title, body, user, privacy, wsc){
+	console.log('addposts()...');
+	let nickname;
+	console.log(title, body, user);
+	if (title.length == 0 || body.length == 0) {
+		wsc.send(JSON.stringify({ type: 'info', where: 'main', statusinfo: 'add post reject' }));
+		console.log("empty input");
+		return;
+	}
+	try{
+		nickname = await db.query("SELECT nickname FROM users WHERE acount = ?", [user]);
+	}
+	catch(e){
+		console.log('Error:', e)
+	}
+	try{
+		db.query("INSERT INTO posts (title, body, user, nickname, privacy) VALUES (?, ?, ?, ?, ?)", [title, body, user, nickname[0].toString(), privacy]);
+		console.log('addposts success');
+		let posts = await listpost(user);
+		wsc.send(JSON.stringify({ type: 'info', where: 'unknown', statusinfo: 'goMain', posts: posts }));
+	}
+	catch(e){
+		console.log('Error:', e);
+		wsc.send(JSON.stringify({ type: 'info', where: 'main', statusinfo: 'add post error' }));
+	}
+	console.log('addposts()...end');
+}
 
 console.log('start at : http://127.0.0.1:8000')
 await app.listen({ port: 8000 });
--- finalExamProject/public/index.js
+++ finalExamProject/public/index.js
@@ -10,8 +10,8 @@ function html2node(html) {
 
 var sendcounter = 0
 
-function send(msg){
-    if (socket.readyState == 1){
+function send(msg) {
+    if (socket.readyState == 1) {
         sendcounter++
         console.log(`send()...${sendcounter} times Success`)
         socket.send(JSON.stringify(msg))
@@ -24,36 +24,210 @@ function send(msg){
     }
 }
 
-function submitform() {
+function submitformlogin() {
     sendcounter = 0
-    console.log('submitform()...') 
+    console.log('submitform()...')
     user = document.querySelector('#username').value
     password = document.querySelector('#password').value
     console.log(user, ":", password) //remove before release
-    send({type:'login', user: user, password: password})
+    send({ type: 'login', user: user, password: password })
     document.querySelector('#password').value = ''
     document.querySelector('#username').value = ''
 }
 
-socket.onopen = function (event) { console.log('socket:onopen()...') }
+function submitformregister() {
+    sendcounter = 0
+    console.log('submitform()...') 
+    user = document.querySelector('#username').value
+    nickname = document.querySelector('#nickname').value
+    password = document.querySelector('#password').value
+    passwordagain = document.querySelector('#password2').value
+    if (password != passwordagain){
+        document.querySelector('#errormsg').innerHTML = 'Password not match'
+        return
+    }
+    console.log(user, ":", password, ':', nickname) //remove before release
+    send({type:'register', user: user, password: password, nickname: nickname})
+    document.querySelector('#password').value = ''
+    document.querySelector('#username').value = ''
+    document.querySelector('#nickname').value = ''
+    document.querySelector('#password2').value = ''
+}
+
+function submitpost() {
+    let title = document.querySelector('#posttitle').value
+    let content = document.querySelector('#postcontent').value
+    let privacy = document.querySelector('#privacy').checked
+    console.log(title, ":", content, ":", privacy) //remove before release
+    send({type:'addpost', title: title, body: content, user: user, privacy: privacy})
+    closepost()
+}
+
+function addposts() {
+    document.querySelector('.addpost').style.display = 'block'
+}
+
+function closepost() {
+    document.querySelector('#posttitle').value = ''
+    document.querySelector('#postcontent').value = ''
+    document.querySelector('#privacy').checked = true
+    document.querySelector('.addpost').style.display = 'none'
+}
+
+function requestLogin() {
+    send({ type: 'changeHTML', where: 'unknown', to: 'login' })
+}
+
+function requestRegister() {
+    send({ type: 'changeHTML', where: 'unknown', to: 'register' })
+}
+
+
+socket.onopen = function (event) {
+    console.log('socket:onopen()...')
+    send({ type: 'changeHTML', where: 'unknown', to: 'login' })
+}
 socket.onerror = function (event) { console.log('socket:onerror()...') }
 socket.onclose = function (event) { console.log('socket:onclose()...') }
 socket.onmessage = function (event) {
     var msg = JSON.parse(event.data)
-    console.log('socket:onmessage()...',msg)
-    switch (msg.type){
+    console.log('socket:onmessage()...', msg)
+    switch (msg.type) {
         case 'info':
-            switch (msg.where){
+            switch (msg.where) {
                 case 'login':
-                    switch (msg.statusinfo){
+                    switch (msg.statusinfo) {
+                        default:
+                            document.querySelector('#errormsg').innerHTML = msg.statusinfo
+                            break
+                    }
+                    break;
+                case 'main':
+                    switch (msg.statusinfo) {
                         case 'success':
                             console.log('login success')
-                            window.location.href = `/main.html`
+                            break
+                        default:
+                            console.log(msg.statusinfo);
+                            break
+                    }
+                    break;
+                case 'register':
+                    switch (msg.statusinfo) {
+                        case 'success':
+                            console.log('register success')
+                            document.querySelector('#errormsg').innerHTML = 'Register Success'
                             break
                         default:
                             document.querySelector('#errormsg').innerHTML = msg.statusinfo
                             break
                     }
+                    break;
+                case 'unknown':
+                    switch (msg.statusinfo) {
+                        case 'goLogin':
+                            console.log(msg.statusinfo)
+                            goLogin()
+                            break
+                        case 'goRegister':
+                            console.log(msg.statusinfo)
+                            goRegister()
+                            break
+                        case 'goMain':
+                            console.log(msg.statusinfo)
+                            goMain(msg.posts);
+                            break
+                        case 'whoareyou':
+                            console.log(msg.statusinfo);
+                            iam();
+                            break
+                        default:
+                            console.log(msg.statusinfo);
+                            break
+                    }
+                    break;
             }
     }
+}
+
+function goLogin() {
+    document.querySelector('#content').innerHTML = `
+    <link rel="stylesheet" href="index.css">
+    <div class = "login">
+        <h1>Blog登入</h1>
+        <p1 id="errormsg"></p1>
+        <form onsubmit="return false;">
+        <input type="text" id="username" placeholder="輸入帳號">
+        <input type="password" id="password" placeholder="輸入密碼" autocomplete="off">
+        <button id='loginbutton' onclick="submitformlogin()">登入</button>
+        </form>
+        <div class="register">
+            <p>還沒有帳號？</p>
+            <a onclick="requestRegister()">註冊</a>
+        </div>
+    </div>
+    `
+}
+
+function goRegister() {
+    document.querySelector('#content').innerHTML = `
+    <link rel="stylesheet" href="index.css">
+    <div class = "login">
+        <h1>Blog註冊</h1>
+        <p1 id="errormsg"></p1>
+        <form onsubmit="return false;">
+        <input type="text" id="username" placeholder="輸入帳號">
+        <input type="text" id="nickname" placeholder="輸入暱稱">
+        <input type="password" id="password" placeholder="輸入密碼">
+        <input type="password" id="password2" placeholder="再次輸入密碼">
+        <button id="loginbutton" onclick="submitformregister()">註冊</button>
+        </form>
+        <div class="register">
+            <p>已經有帳號？</p>
+            <a onclick="requestLogin()">登入</a>
+        </div>
+    </div>
+    `
+}
+
+function goMain(posts) {
+    let list = [];
+    for (let post of posts) {
+        list.push(`
+        <div id = "post" class = "posts">
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <p class="usertime">${post.nickname} 在${post.timestamp}寫到</p>
+        </div>
+        `)
+    }
+    document.querySelector('#content').innerHTML = `
+    <link rel="stylesheet" href="main.css">
+    <h1 id = "titleh1">Blog</h1>
+    <ul class = "navigationbar">
+        <li><a onclick="">貼文</a></li>
+        <li><a onclick="">聊天</a></li>
+        <li><a onclick="requestLogin()" class="userinfo">登出</a></li>
+        <li><a onclick="" id = "username" class="userinfo">${user}</a></li>
+    </ul>
+    ${list.join('\n')}
+    <div onclick="addposts()" class="add">
+        <p>+</p>
+    </div>
+    <div class="addpost">
+        <form onsubmit="return false;">
+            <p>新增貼文</p>
+            <div>
+                <input type="checkbox" id="privacy" checked>
+                <label>私人</label>    
+            </div>
+            <input type="text" id="posttitle" placeholder="輸入標題">
+            <textarea id="postcontent" placeholder="輸入內容"></textarea>
+            <div>
+                <button id="cancelbutton" onclick="closepost()">取消</button>
+                <button id="submitbutton" onclick="submitpost()">發布</button>
+            </div>
+        </form>
+    </div>
+    `
 }
